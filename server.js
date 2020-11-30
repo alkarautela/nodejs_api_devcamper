9cafc6d169a89053535e2959284cabe68be7c1eb
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-11-30 14:56:11 +0530

    initial express setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+
+//Load env variables
+dotenv.config({path: './config/config.env'});
+
+//initialize app variable with express
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+
+
+
