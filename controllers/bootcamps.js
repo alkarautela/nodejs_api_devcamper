6894c627f0c8468ea5a1dcaf52404519d2899754
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-11 00:20:25 +0530

    sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,9 +13,9 @@ exports.getBootcamps = asyncHandler( async(req, res, next) => {  //these are mid
     const reqQuery = {...req.query};
 
     // Fields to exclude
-    const removeFields = ['select']
+    const removeFields = ['select', 'sort']
 
-    // Loop ove removeFields and delete then from reqQuery
+    // Loop over removeFields and delete then from reqQuery
     removeFields.forEach( param => delete reqQuery[param]);
 
 
@@ -32,6 +32,14 @@ exports.getBootcamps = asyncHandler( async(req, res, next) => {  //these are mid
     if(req.query.select){
         const fields = req.query.select.split(',').join(' ');
         query = query.select(fields)
+    }
+
+    // Sort
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(" ");
+        query = query.sort(sortBy);
+    }else{
+        query = query.sort('-createdAt');
     }
 
     // Executing query
