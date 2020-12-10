bb69d4c268bcd7c25f25a5e01e5a113156ada0d8
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-11 00:59:16 +0530

    Course Model and Seeding

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to out db
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,10 +24,15 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Imports into DB
 const importData = async() => {
     try{
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
 
         console.log('Data Imported...'.green.inverse);
         process.exit()
@@ -39,6 +45,7 @@ const importData = async() => {
 const deleteData = async() => {
     try{
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse);
         process.exit()
