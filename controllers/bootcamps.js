9c6249e67f66b21535f4f5b6aed5b38c7846f1f0
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-10 07:43:54 +0530

    Mongoose error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,7 @@ exports.getBootcamps = async(req, res, next) => {  //these are middleware functi
         const bootcamps = await Bootcamp.find()
         res.status(200).json({success: true, count: bootcamps.length, data: bootcamps})
     }catch(err){
-        res.status(400).json({success: false})
+        next(err)
     }
 }
 
@@ -28,7 +28,8 @@ exports.getBootcamp = async(req, res, next) => {
         
         res.status(200).json({success: true, data: bootcamp})
     }catch(err){
-        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)) //this will fire if id is not a formatted object id
+        // next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)) //this will fire if id is not a formatted object id
+        next(err)
     }
 }
 
@@ -47,7 +48,7 @@ exports.createBootcamp = async(req, res, next) => {
             data: bootcamp
         })
     }catch(err){
-        res.status(400).json({success: false})
+        next(err)
     }
 }
 
@@ -64,12 +65,12 @@ exports.updateBootcamp = async(req, res, next) => {
         });
 
         if(!bootcamp){
-            return res.status(400).json({success: false})
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
         }
 
         res.status(200).json({success: true, data: bootcamp})
     }catch(err){
-        res.status(403).json({success: false})
+        next(err)
     }
 }
 
@@ -82,11 +83,11 @@ exports.deleteBootcamp = async(req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if(!bootcamp){
-            return res.status(400).json({success: false})
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
         }
 
         res.status(200).json({success: true, data: {}})
     }catch(err){
-        res.status(403).json({success: false})
+        next(err)
     }
 }
--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,34 @@
+const ErrorResponse = require('../utils/errorResponse')
+
 //express error handler
 const errorHandler = (err, req, res, next) => {
-    // Log to the console for dev
-    console.log(err.stack.red);
+    let error  = {...err}
 
-    res.status(err.statusCode || 500).json({
+    error.message = err.message
+    // Log to the console for dev
+    console.log(err.errors);
+
+    // Mongoose bad ObjectId
+    if(err.name === 'CastError'){
+        const message = `Bootcamp not found with id of ${err.value}`
+        error = new ErrorResponse(message, 404)
+    }
+
+    // Mongoose duplicate key
+    if(err.code === 11000){
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400)
+    }
+
+    // Mongoose Validation error
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400)
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     })
 }
 
