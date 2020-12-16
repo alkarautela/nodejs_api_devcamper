5f504df5fd85b87aeae07ff6fa8bbf17b4ea9108
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-16 13:02:14 +0530

    User Register and Encrypting Password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -32,5 +33,11 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 })
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);  // genSalt method takes number of rounds >> higher the round == more secure password  BUT heavier it is on our system, so 10 is the recommended  
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 module.exports = mongoose.model('User', UserSchema)
