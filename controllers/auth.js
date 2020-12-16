5756db3b96defe7b3bc36f23e7e8b3b9442d3936
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-16 14:39:49 +0530

    User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async')
 const User = require('../models/User')
 
 // @desc        Register User
-//@route        GET /api/v1/auth/register
+//@route        POST /api/v1/auth/register
 //@access       Public
 exports.register = asyncHandler(async (req, res, next) => {
     const {name, email, password, role} = req.body;
@@ -16,7 +16,40 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     });
 
-    // Create token
+    // Create token // check in jwt.io by passing token 
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token});
+})
+
+
+// @desc        Login User
+//@route        POST /api/v1/auth/login
+//@access       Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    // Validate Email and Password
+    if(!email || !password){
+        return next(new ErrorResponse('Please provide an email and password', 400))
+    }
+
+    // Check for user
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user){
+        return next(new ErrorResponse('Invalid Credentials', 401))
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+        return next(new ErrorResponse('Invalid Credentials', 401))
+    }
+
+
+    // Create token // check in jwt.io by passing token 
     const token = user.getSignedJwtToken();
 
     res.status(200).json({success: true, token});
