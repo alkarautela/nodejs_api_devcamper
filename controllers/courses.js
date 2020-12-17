180c029dc0a40f4902698e7866e5359434622953
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-17 08:01:01 +0530

    Course Ownership

--- controllers/courses.js
+++ controllers/courses.js
@@ -51,12 +51,22 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.addCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId;
 
+    req.body.user = req.user.id;
+
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
     if(!bootcamp){
         return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`), 404)
     }
 
+    // Make sure course owner is the user that is logged in
+    // Make sure user is course owner
+    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(
+            new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401)
+        )
+    } 
+    
     const course = await Course.create(req.body);
 
     res.status(200).json({
@@ -75,6 +85,13 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     if(!course){
         return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
     }
+
+    // Make sure user is course owner
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(
+            new ErrorResponse(`User ${req.user.id} is not authorized to update course ${course._id}`, 401)
+        )
+    } 
 
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
@@ -97,6 +114,13 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
     if(!course){
         return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404)
     }
+
+    // Make sure user is course owner
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(
+            new ErrorResponse(`User ${req.user.id} is not authorized to delete course ${course._id}`, 401)
+        )
+    } 
 
     await course.remove()
 
