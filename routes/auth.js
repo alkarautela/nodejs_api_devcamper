5453805148b3039570f2a0af8dc66406e27d2db5
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-17 21:30:21 +0530

    update user's name and email

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require('express')
-const {register, login, getMe, forgotPassword, resetPassword} = require('../controllers/auth')
+const { 
+    register, 
+    login, 
+    getMe, 
+    forgotPassword, 
+    resetPassword,
+    updateDetails
+} = require('../controllers/auth')
 
 const router = express.Router();
 
@@ -7,6 +14,7 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me',protect, getMe);
+router.put('/updatedetails',protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
