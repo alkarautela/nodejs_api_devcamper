ce3787785a48671396e6914c14a4b869f36e9af3
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-12-17 12:15:21 +0530

    Forgot Password - Send Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
 const User = require('../models/User')
 
 // @desc        Register User
@@ -76,12 +77,31 @@ exports.forgotPassword = asyncHandler( async (req, res, next) => {
     // Get reset token
     const resetToken = user.getResetPasswordToken();
 
-    await user.save({validateBeforeSave: false});
+    await user.save({validateBeforeSave: false});  // because we do not want to run the other validators for user data.
 
-    res.status(200).json({
-        success: true, 
-        data: user
-    })
+    // create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    // do this using frontend not like this
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password.
+    Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try{
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+        res.status(200).json({success: true, data: 'Email sent'});
+    }catch(err){    
+        console.log(err);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpired = undefined;
+
+        await user.save({validateBeforeSave: false})
+
+        return next(new ErrorResponse("Email could not be sent", 500))
+    }
 })
 
 
